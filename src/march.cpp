8e00134c4baeba94cb9c97e6e3ce37badc6b3c42
andrew blinn <me@disconcision.com>
2019-03-23 02:41:19 -0400

    refactored march

--- src/march.cpp
+++ src/march.cpp
@@ -11,6 +11,7 @@
 
 R march(const Ray& ray, R (&sdf)(R3),
         unsigned& steps, unsigned& hit_id) {
+
   /* march along a ray towards towards the sdf
    * starting at depth min_d and returning the
    * depth to level set surface, up to max_d */
@@ -20,10 +21,7 @@ R march(const Ray& ray, R (&sdf)(R3),
   for (steps=0; steps < MARCH_MAX_STEPS; steps++) {
     R3 point = follow(ray, depth);
     R distance = sdf(point);
-    if (distance < EPSILON_MARCH) {
-
-      return depth;
-    }
+    if (distance < EPSILON_MARCH) break;
     depth += distance;
     if (depth >= MAX_D) break;
   }
